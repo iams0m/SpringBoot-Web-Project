9a55dc1d2d904a47bede0aeae4dfc2c26c05e28a
iams0m <n980805@gmail.com>
2024-08-03 01:04:16 +0900

    test: 페이징 처리 검증을 위한 테스트 코드 생성

--- ex2/src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
+++ ex2/src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
@@ -3,6 +3,9 @@ package org.zerock.ex2.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.zerock.ex2.entity.Memo;
 
@@ -66,5 +69,28 @@ public class MemoRepositoryTests {
     public void testDelete() {
         Long mno = 100L;
         memoRepository.deleteById(mno);
+    }
+
+    // 페이징 처리 테스트
+    @Test
+    public void testPageDefault() {
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<Memo> result = memoRepository.findAll(pageable);
+        System.out.println(result);
+
+        System.out.println("----------------------------------------------");
+
+        System.out.println("Total Pages: " + result.getTotalPages()); // 총 페이지 수
+        System.out.println("Total Count: " + result.getTotalElements()); // 전체 개수
+        System.out.println("Page Number: " + result.getNumber()); // 현재 페이지 번호
+        System.out.println("Page Size: " + result.getSize()); // 페이지당 데이터 개수
+        System.out.println("has next page?: " + result.hasNext()); // 다음 페이지 존재 여부
+        System.out.println("first page?: " + result.isFirst());// 시작 페이지(0) 여부
+
+        System.out.println("----------------------------------------------");
+
+        for (Memo memo : result.getContent()) {
+            System.out.println(memo);
+        }
     }
 }
